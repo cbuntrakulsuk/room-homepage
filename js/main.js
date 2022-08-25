8033852e48d0bd5192e5446a9ae3c8dcce1e7271
Chad Buntrakulsuk <chad.buntrakulsuk@gmail.com>
2022-08-25 11:27:54 -0700

    experimental slider

--- js/main.js
+++ js/main.js
@@ -14,18 +14,18 @@ var mobileSlider = tns({
   nav: false,
 });
 
-var desktopSlider = tns({
-  container: ".desktop-slider",
-  items: 1,
-  speed: 500,
-  navcontainer: ".nav-arrows",
-  controls: 1,
-  autoplayButtonOutput: 0,
-  autoplay: true,
-  nextButton: "#right-arrow-d",
-  prevButton: "#left-arrow-d",
-  nav: false,
-});
+// var desktopSlider = tns({
+//   container: ".desktop-slider",
+//   items: 1,
+//   speed: 500,
+//   navcontainer: ".nav-arrows",
+//   controls: 1,
+//   autoplayButtonOutput: 0,
+//   autoplay: true,
+//   nextButton: "#right-arrow-d",
+//   prevButton: "#left-arrow-d",
+//   nav: false,
+// });
 
 // Open mobile menu
 let openButton = document.querySelector("#hamburger-menu");
@@ -41,4 +41,26 @@ let mobileMenu = document.querySelector("#mobile-menu");
 closeButton.addEventListener("click", () => {
   mobileMenu.style.display = "none";
   overlay.style.display = "none";
+});
+
+let nexSlide = document.querySelector("#nextArrow");
+nexSlide.addEventListener("click", () => {
+  let activeSlide = document.querySelector(".slide.translate-x-0");
+  activeSlide.classList.remove("translate-x-0");
+  activeSlide.classList.add("-translate-x-full");
+
+  let nextSlide = activeSlide.nextElementSibling;
+  nextSlide.classList.remove("translate-x-full");
+  nextSlide.classList.add("translate-x-0");
+});
+
+let previousSlide = document.querySelector("#prevArrow");
+previousSlide.addEventListener("click", () => {
+  let activeSlide = document.querySelector(".slide.translate-x-0");
+  activeSlide.classList.remove("translate-x-0");
+  activeSlide.classList.add("translate-x-full");
+
+  let previousSlide = activeSlide.previousElementSibling;
+  previousSlide.classList.remove("-translate-x-full");
+  previousSlide.classList.add("translate-x-0");
 });
