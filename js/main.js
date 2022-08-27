cf74396d73b676b8e540256b6c9b66ebbc1e5269
Chad Buntrakulsuk <chad.buntrakulsuk@gmail.com>
2022-08-26 22:11:43 -0700

    fix slider

--- js/main.js
+++ js/main.js
@@ -43,24 +43,11 @@ closeButton.addEventListener("click", () => {
   overlay.style.display = "none";
 });
 
-let nexSlide = document.querySelector("#nextArrow");
-nexSlide.addEventListener("click", () => {
-  let activeSlide = document.querySelector(".slide.translate-x-0");
-  activeSlide.classList.remove("translate-x-0");
-  activeSlide.classList.add("-translate-x-full");
+//new slider
 
-  let nextSlide = activeSlide.nextElementSibling;
-  nextSlide.classList.remove("translate-x-full");
-  nextSlide.classList.add("translate-x-0");
-});
+const sliderImages = document.querySelectorAll(".slide");
+let counter = 0;
 
-let previousSlide = document.querySelector("#prevArrow");
-previousSlide.addEventListener("click", () => {
-  let activeSlide = document.querySelector(".slide.translate-x-0");
-  activeSlide.classList.remove("translate-x-0");
-  activeSlide.classList.add("translate-x-full");
-
-  let previousSlide = activeSlide.previousElementSibling;
-  previousSlide.classList.remove("-translate-x-full");
-  previousSlide.classList.add("translate-x-0");
+sliderImages.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
 });
